db48fa214f4ede1d6a4f417dd0d52cfb57a0c852
ray2928 <rxpkd@mail.missouri.edu>
2015-02-14 23:08:54 -0600

    Implement persistently save data. Next: try to configure the core data editor

--- SleepTracker/SleepTrackerViewController.m
+++ SleepTracker/SleepTrackerViewController.m
@@ -22,9 +22,40 @@
     NSDateFormatter *dateformatter = [[NSDateFormatter alloc]init];
     [dateformatter setDateFormat:@"MM-dd-yyyy"];
     NSString* currDate = [dateformatter stringFromDate:date];
-    NSLog(@"date is %@",currDate);
+    //NSLog(@"date is %@",currDate);
     return currDate;
 }
+
+// write the time time and awake time persistently
+- (IBAction)SleepRecord:(UIButton *)sender {
+    NSDate *currentTime = self.TimePicker.date;
+    NSDateFormatter *timeFormatter = [[NSDateFormatter alloc]init];
+    [timeFormatter setDateFormat:@"HH:mm"];
+    NSString* currTime = [timeFormatter stringFromDate:currentTime];
+    NSLog(@"time is %@", currTime);
+    //record sleepTime
+    NSManagedObjectContext *context = [self manageObjectContext];
+    NSManagedObject *newSleepTime = [NSEntityDescription insertNewObjectForEntityForName:@"User" inManagedObjectContext:context];
+    [newSleepTime setValue:currentTime forKey:@"sleepDate"];
+    
+    NSError *error = nil;
+    if (![context save:&error]) {
+        NSLog(@"Can't save! %@ %@", error, [error localizedDescription]);
+    }
+    
+}
+
+- (NSManagedObjectContext *) manageObjectContext {
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+
+
+
 
 - (void) viewDidLoad{
     [super viewDidLoad];
